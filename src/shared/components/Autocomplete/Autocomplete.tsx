e7a711ed554eaecb5a2c785be91a2fad30e562fb
Pablo Maffioli <pablo.maff.076@gmail.com>
2025-07-22 19:21:33 +0200

    'Autocomplete > Displaying the list > shows the dropdown when input is focused and items are provided' GREEN

--- src/shared/components/Autocomplete/Autocomplete.tsx
+++ src/shared/components/Autocomplete/Autocomplete.tsx
@@ -1,3 +1,26 @@
-export function Autocomplete() {
-  return <div></div>;
+import { SearchInput } from '@components/SearchInput/SearchInput';
+
+interface AutocompleteProps {
+  items: string[];
+  onSearch: (searchValue: string) => string;
+}
+
+export function Autocomplete({ items, onSearch }: AutocompleteProps) {
+  return (
+    <div role="combobox" aria-haspopup="listbox">
+      <SearchInput
+        onSearch={onSearch}
+        debounce
+        aria-autocomplete="list"
+        aria-controls="autocomplete-list"
+      />
+      <ul role="listbox" id="autocomplete-list">
+        {items.map((item) => (
+          <li role="option" id={item} key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
