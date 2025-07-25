5bab5a085014cd7567071943218fe1073d6b7433
Pablo Maffioli <pablo.maff.076@gmail.com>
2025-07-25 22:58:04 +0200

    simplify Input props type

--- src/shared/components/Input/Input.tsx
+++ src/shared/components/Input/Input.tsx
@@ -1,6 +1,6 @@
 import type { InputHTMLAttributes } from 'react';
 import styles from './Input.module.scss';
 
-export function Input({ value, onChange, ...props }: InputHTMLAttributes<HTMLInputElement>) {
-  return <input className={styles.input} value={value} onChange={onChange} {...props} />;
+export function Input({ ...props }: InputHTMLAttributes<HTMLInputElement>) {
+  return <input className={styles.input} {...props} />;
 }
