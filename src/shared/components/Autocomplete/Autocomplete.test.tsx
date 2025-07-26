c348641c93e7f45efb8e3acc235d3b909d886269
Pablo Maffioli <pablo.maff.076@gmail.com>
2025-07-26 14:10:11 +0200

    'Autocomplete > Navigating items with the keyboard > if input has been autocompleted with arrows, when right arrow is pressed inputs goes back to what the user typed' RED

--- src/shared/components/Autocomplete/Autocomplete.test.tsx
+++ src/shared/components/Autocomplete/Autocomplete.test.tsx
@@ -449,16 +449,16 @@ describe('Autocomplete', () => {
       expect(input).toHaveValue('abc');
     });
 
-    it.skip('if input has been autocompleted with arrows, when right arrow is pressed inputs goes back to what the user typed', async () => {
+    it('if input has been autocompleted with arrows, when right arrow is pressed inputs goes back to what the user typed', async () => {
       const { getByRole } = render(<Autocomplete items={testItems} onSelect={handleOnSelect} />);
 
       const input = getByRole('textbox', { name: 'search' });
 
       await user.type(input, 'abc');
 
-      await user.keyboard('{ArrowDown}{ArrowLeft}');
+      await user.keyboard('{ArrowDown}{ArrowRight}');
 
-      expect(input).toHaveValue(testItems[0]);
+      expect(input).toHaveValue('abc');
     });
   });
 
