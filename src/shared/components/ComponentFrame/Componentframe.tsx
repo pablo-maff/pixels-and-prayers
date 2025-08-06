24c2542d405de4807809df2877965d9defe2e227
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-08-06 16:22:32 +0200

    fix: conditionally render title in ComponentFrame to avoid empty <h4>

--- src/shared/components/ComponentFrame/Componentframe.tsx
+++ src/shared/components/ComponentFrame/Componentframe.tsx
@@ -10,7 +10,7 @@ interface ComponentFrameProps {
 export default function ComponentFrame({ title, children, className = '' }: ComponentFrameProps) {
   return (
     <div className={className}>
-      <h4 className={style.h4}>{title}</h4>
+      {!!title?<h4 className={style.h4}>{title}</h4>: null}
       <div className={style.componentFrame}>{children}</div>
     </div>
   );
