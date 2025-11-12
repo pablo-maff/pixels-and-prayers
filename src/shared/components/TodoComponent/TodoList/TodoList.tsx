1ec06f90f4b69e0e6aa76388a18f6e574dce51ea
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-11-12 18:30:53 +0100

    feat(TodoList):add scoped style

--- src/shared/components/TodoComponent/TodoList/TodoList.tsx
+++ src/shared/components/TodoComponent/TodoList/TodoList.tsx
@@ -1,11 +1,12 @@
 import { TodoItem } from "../TodoItem/TodoItem";
 import type { TodoItemProps } from "../TodoItem/TodoItem";
+import style from "./TodoList.module.scss"
 
 export function TodoList({ todos }: { todos: TodoItemProps[] }) {
   return (
     <>
       {todos && todos.length > 0 ? (
-        <ul>
+        <ul className={style.list_element}>
           {todos.map((item) => (
             <TodoItem item={item} key={item.id}/>
           ))}
